06db9297c432362eb1c92a5f932720042fd3b450
sabine <drsabinethomas@gmail.com>
2018-09-06 23:23:08 -0700

    Wave 2 Rental initialization and cost methods completed

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,14 +1,12 @@
 require 'time'
 
-
 module GreenBox
   class DateRange
-    attr_reader :start_date , :end_date
+    attr_reader :start_date , :end_date, :nights
 
     def initialize(start_date, end_date)
       @start_date = start_date
       @end_date = end_date
-
 
       if start_date > end_date
         raise ArgumentError.new "The end date is before the start date"
@@ -16,30 +14,27 @@ module GreenBox
     end
 
     def contains(date)
-      if (date >= start_date) && (date < end_date)
+      if (date >= @start_date) && (date < @end_date)
         return true
       else
-        date < start_date || date >= end_date
+        date < @start_date || date >= @end_date
         return false
       end
     end
 
-
     def overlaps(other_date_range)
       if contains(other_date_range.start_date) || contains(other_date_range.end_date)
         true
-      elsif other_date_range.contains(start_date) && other_date_range.contains(end_date)
+      elsif other_date_range.contains(@start_date) && other_date_range.contains(@end_date)
         true
       else
         false
       end
     end
 
-
     def nights
-      count = (end_date - start_date)/(60*60*24)
+      count = (@end_date - @start_date)/(60*60*24)
       return count
     end
-
   end
 end
--- lib/rental.rb
+++ lib/rental.rb
@@ -4,37 +4,25 @@ require_relative 'movie'
 
 module GreenBox
   class Rental
-    # attr_reader :movie, :start_date, :end_date, :customer
-    attr_writer :movie, :date_range , :customer
+    attr_accessor :movie, :date_range, :customer
 
-
-    # def initialize(movie, start_date, end_date, customer)
-    def initialize(movie, date_range = end_date - start_date, customer)
-      @movie = movie
-      # @start_date = Time.parse(start_date)
-      # @end_date = Time.parse(end_date)
-      @date_range = Time.parse.date_range
-      @customer = customer
-
-
-    # For future I would say make an instance of a date_range class and call a
-      # date_range =
-      if date_range = ('2018-08-09' - '2018-08-09')
+    def initialize(movie, date_range, customer)
+      if date_range.nights <= 0
         raise ArgumentError.new "This is an illegal date range"
       end
-
+      @movie = movie
+      @date_range = date_range
+      @customer = customer
     end
-   # rental = GreenBox::Rental
-   rental = GreenBox::Rental.new("Princess Bride", 3, "Abi")
 
     def cost
-      count = (end_date - start_date)/(60*60*24)
-      cost = count * 3
-      return "The cost of renting a movie for #{count} days is $ #{cost}"
+      # date_range.nights returns an integer of total nights 
+      # need to subtract one night (day) to calculate correct cost
+      rent = @date_range.nights
+      # cost is number of days multipled by three dollars (per day) 
+      cost = rent * 3.0
+      # rounding should return the correct cents. 
+      return cost.round(2) 
     end
-
-
   end
 end
-
-# rental = GreenBox::Rental.new("Princess Bride", '3', "Abi")
